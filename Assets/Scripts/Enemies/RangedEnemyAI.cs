144b433898741c180374a78c9827b9b80056aa51
PebbleBuilds <kiribuilds@gmail.com>
2021-10-05 12:55:33 -0400

    Collisions are... better

--- Assets/Scripts/Enemies/RangedEnemyAI.cs
+++ Assets/Scripts/Enemies/RangedEnemyAI.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RangedEnemyAI : MonoBehaviour
 {
+    private GameObject player;
+    private Rigidbody myRigidbody;
+    private Vector3 unitVectTowardPlayer;
+
+
+    [Header("Movement")]
+    public float walkImpulse = 8.0f;
+    public float walkMaxSpeed = 8.0f;
+    public float absMaxSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Check if player is still alive
+        if (player == null)
+        {
+            // Poll for player
+            player = GameObject.FindWithTag("Player");
+        }
+        else
+        {
+            // Calculate unit vect toward player
+            unitVectTowardPlayer = (player.transform.position - this.transform.position).normalized;
+
+            // Get rid of the y component
+            unitVectTowardPlayer.y = 0;
+            unitVectTowardPlayer = unitVectTowardPlayer.normalized;
+
+            // Only walk if not already moving fast enough toward player
+            if (Vector3.Dot(myRigidbody.velocity, unitVectTowardPlayer) < walkMaxSpeed)
+            {
+                myRigidbody.velocity = new Vector3(unitVectTowardPlayer.x * walkImpulse, myRigidbody.velocity.y, unitVectTowardPlayer.z * walkImpulse);
+            }
+        }
+
+        // Should not move faster than absMaxSpeed
+        if(myRigidbody.velocity.magnitude <= absMaxSpeed)
+        {
+            myRigidbody.velocity = myRigidbody.velocity.normalized * absMaxSpeed;
+        }
     }
 }
