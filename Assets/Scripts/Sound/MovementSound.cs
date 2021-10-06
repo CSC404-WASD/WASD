b2d37e38fd8ec936c27cfcf94f1b0b18802c228b
KobiJames <jamesjhirad@gmail.com>
2021-10-05 23:58:32 -0400

    Update MovementSound.cs

--- Assets/Scripts/Sound/MovementSound.cs
+++ Assets/Scripts/Sound/MovementSound.cs
@@ -10,24 +10,26 @@ public class MovementSound : SoundEffect
         audiosource = FindObjectOfType<AudioSource>();
         audiosource.loop = true;
         audiosource.clip = clips[0];
+        audiosource.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Movement
-        if(Input.GetAxisRaw("Horizontal")!=0 || Input.GetAxisRaw("Vertical")!=0){
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
             if(!audiosource.isPlaying){
-                /*counter += 1;
-                if(counter == clips.Length){
-                    counter = 0;
-                }*/
                 audiosource.clip = clips[0];//Random.Range(0, clips.Length)];
                 audiosource.Play();
             }
-        }else{
-            audiosource.Stop();
+        } else{
+
+            /*if(audiosource.isPlaying){
+                audiosource.Stop();
+            }*/
+
         }
+
 
     }
 }
